c45c63c78c638a404a6d759c9da834d97d98743a
osaigbovo idiaghe <idiagheosaigbovoosas@gmail.com>
2024-06-06 10:57:44 +0100

    vue-day-one: relearning

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <main>
+    <div>
+      <h4>count is....</h4>
+      <h1>{{ count }}</h1>
+      <button @click="count++">Increment</button>
+      <button @click="count--">Decrement</button>
+    </div>
+  </main>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+
+const count = ref(0);
+</script>
+
+<style scoped>
+main {
+  background-color: rgb(232, 241, 238);
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+div {
+  text-align: center;
+}
+</style>
